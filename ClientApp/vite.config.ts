854bfb1b43dc6657be06c1f8689dce4189a37bc6
David W. Gray <dwgray67@hotmail.com>
2023-11-07 14:42:56 -0800

    Fix plugin and bad comments

--- ClientApp/vite.config.ts
+++ ClientApp/vite.config.ts
@@ -1,13 +1,13 @@
-import { fileURLToPath, URL } from 'node:url'
-import { defineConfig, Plugin, UserConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import mkcert from 'vite-plugin-mkcert'
-import { dirname, resolve } from 'path'
-import Inspect from 'vite-plugin-inspect'
-import fg from 'fast-glob'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig, normalizePath, Plugin, UserConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import mkcert from 'vite-plugin-mkcert';
+import { dirname, resolve } from 'path';
+import Inspect from 'vite-plugin-inspect';
+import fg from 'fast-glob';
 
 // @ts-ignore
-import appsettingsDev from '../appsettings.Development.json'
+import appsettingsDev from '../appsettings.Development.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -18,21 +18,21 @@ export default defineConfig({
     https: true,
     strictPort: true,
     hmr: {
-      clientPort: appsettingsDev.Vite.Server.Port
-    }
+      clientPort: appsettingsDev.Vite.Server.Port,
+    },
   },
   build: {
     outDir: '../wwwroot/vite-client',
     emptyOutDir: true,
     manifest: true,
-    cssCodeSplit: true
+    cssCodeSplit: true,
   },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
 
 // This plugin will find all files matching the pattern src/pages/*/App.vue
 //  and make endpoints with the 'name' being the replacement for the *. and the
@@ -45,34 +45,40 @@ import { createApp } from 'vue'
 import App from '{app}'
 
 createApp(App).mount('#app')
-`
+`;
 
   return {
     name: 'auto-endpoints',
     async config(): Promise<UserConfig> {
-      const root = 'src/pages/'
-      const pattern = root + '*/Vue.app'
-      const length = root.length
-      const dirs = (await fg.glob(pattern)).map((p) => dirname(p).substring(length))
-      console.log(dirs.join(','))
+      const root = 'src/pages/';
+      const pattern = root + '*/App.vue';
+      const length = root.length;
+      const dirs = (await fg.glob(pattern)).map((p) => dirname(p).substring(length));
+      console.log(dirs.join(','));
 
       const input = dirs.reduce((obj, item) => {
-        const value = resolve(__dirname, root + item + '/main.ts')
-        obj[item] = value
-        return obj
-      }, {} as any)
+        const value = resolve(__dirname, root + item + '/main.ts');
+        obj[item] = value;
+        return obj;
+      }, {} as any);
 
       return {
         build: {
           rollupOptions: {
-            input: input
-          }
-        }
-      }
+            input: input,
+          },
+        },
+      };
+    },
+    resolveId(id: string): null | string {
+      return id.endsWith('main.ts') ? id : null;
     },
     load(id: string): null | string {
-      const app = id.replace(/.*\/src\//, '@/').replace('main.ts', 'App.vue')
-      return id.endsWith('main.ts') ? main.replace('{app}', app) : null
-    }
-  }
+      if (!id.endsWith('main.ts')) return null;
+
+      id = normalizePath(id);
+      const app = id.replace(/.*\/src\//, '@/').replace('main.ts', 'App.vue');
+      return main.replace('{app}', app);
+    },
+  };
 }
